473f0aad05651f940392f4e881b46fdb7ce61ee6
Luca Maroni <lcmaroni77@gmail.com>
2017-06-01 16:12:59 +0200

    ehm ...

--- tests/manual/sample.php
+++ tests/manual/sample.php
@@ -9,8 +9,8 @@ include_once '../../vendor/autoload.php';
 // configure api connection and log file
 $configData = [
   'ZOOM_API_BASE_URL' => 'https://api.zoom.us/v1/',
-  'ZOOM_API_SECRET' => 'wm8eOsWsuXdu8dbjCx7fFdqgmJj3o0WfzuGe',
-  'ZOOM_API_KEY' => 'ho2UZbjESjqT12BdDIX6rg',
+  'ZOOM_API_SECRET' => 'your-secret',
+  'ZOOM_API_KEY' => 'your-key',
   'ZOOM_API_DATA_TYPE' => 'JSON'
 ];
 
@@ -20,7 +20,7 @@ $client = new CurlAPIClient($config, $logger);
 
 // test list upcoming webinar with registration mandatory
 $getListWebinar = [
-  'host_id' => 'qOWweBShQwGZjPoC6yVh8g',    // host id of who organize the webinar
+  'host_id' => 'your-host-id-for-webinars',    // host id of who organize the webinar
   'page_size' => 10,                        // pagination
   'page_number' => 1
 ];
@@ -29,8 +29,8 @@ $upcomingWebinars = $client->makePostCall('webinar/list/registration', $getListW
 
 // test make new registration for webinar
 $registrationWebinar = [
-  'id' => 746442357,                    // id webinar
-  'email' => 'l.cmaroni77@gmail.com',    // participant email
+  'id' => 'id-existing-webinar',                    // id webinar
+  'email' => 'fabio.verdi@testmail.com',    // participant email
   'first_name' => 'Fabio',              // participant first name
   'last_name' => 'Verdi'                // participant last name
 ];
